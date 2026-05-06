a81b56b7d9130b8190cf2ad5fe3acb086d9339f8
decabral <deborahcabralsilva@gmail.com>
2026-05-06 17:00:11 -0300

    refactor: fix end node n create imperfect_lab logic

--- mazegen.py
+++ mazegen.py
@@ -1,19 +1,22 @@
 import random
 
+
 class MazeGenerator:
     def __init__(self, config: dict):
         self.width = config['WIDTH']
         self.height = config['HEIGHT']
         self.start_node = config['ENTRY']
-        self.end_node = ['EXIT']
+        self.end_node = config['EXIT']
         self.seed = config.get('SEED', None)
 
         if self.seed is not None:
             random.seed(self.seed)
-        
-        self.grid = [[15 for _ in range(self.width)] for _ in range(self.height)]
 
-        self.visited = [[False for _ in range(self.width)] for _ in range(self.height)]
+        self.grid = [[15 for _ in range(self.width)]
+                     for _ in range(self.height)]
+
+        self.visited = [[False for _ in range(self.width)]
+                        for _ in range(self.height)]
 
     def get_unvisited_neighbors(self, x: int, y: int) -> list:
         neighbors = []
@@ -31,11 +34,18 @@ class MazeGenerator:
                     neighbors.append((nx, ny, wall, opposite_wall))
         return neighbors
 
-
-    def remove_wall(self, current_x, current_y, next_x, next_y, wall, opposite_wall) -> None:
+    def remove_wall(self, current_x, current_y,
+                    next_x, next_y, wall, opposite_wall) -> None:
         self.grid[current_y][current_x] -= wall
         self.grid[next_y][next_x] -= opposite_wall
 
+    def imperfect_lab(self, chance: float) -> None:
+        for y in range(1, self.height - 1):
+            for x in range(1, self.width - 1):
+                if random.random() < chance:
+                    if self.grid[y][x] >= 2:
+                        self.grid[y][x] -= 2
+                        self.grid[y][x + 1] -= 8
 
     def generate(self) -> list:
         start_x, start_y = self.start_node
@@ -48,7 +58,8 @@ class MazeGenerator:
 
             if neighbors:
                 next_x, next_y, wall, opposite_wall = random.choice(neighbors)
-                self.remove_wall(current_x, current_y, next_x, next_y, wall, opposite_wall)
+                self.remove_wall(current_x, current_y,
+                                 next_x, next_y, wall, opposite_wall)
                 self.visited[next_y][next_x] = True
                 stack.append((next_x, next_y))
             else:
